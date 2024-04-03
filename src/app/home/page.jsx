879e055fd56eaefb2d7ba7226e418b972ab605ad
santu jana <santujana.1998@gmail.com>
2024-04-03 18:40:19 +0530

    banner

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,14 +1,29 @@
 "use client"
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Slider from "react-slick";
 import Image from 'next/image'
 import icon from '@/assets/burger.png'
 import { FiShoppingCart, FiUser } from "react-icons/fi"
 import { CiDiscount1 } from "react-icons/ci"
 import { useState } from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import banner1 from '../../../public/images/banner/1.jpg'
+import banner2 from '../../../public/images/banner/2.svg'
+import banner3 from '../../../public/images/banner/3.jpg'
 
 export default function Home(){
     const [serfType, setSerfType] = useState(0);
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000
+      };
     return(
         <>
             <div className="d-flex align-items-center justify-content-between px-3 cursor-pointer" style={ styles.navbar }>
@@ -17,11 +32,14 @@ export default function Home(){
                     <h5 className="">Easy Dine</h5>
                 </div>
 
-                <div className='border rounded border-secondary d-flex overflow-hidden'>
-                    <div className='px-2' style={serfType == 0 ? styles.activeTab : styles.pointer} onClick={()=>setSerfType(0)}>
+                <div className='d-flex'>
+                    <div className='px-2' onClick={()=>setSerfType(0)}>
                         <span>Order Online</span>
                     </div>
-                    <div className='px-2' style={serfType == 1 ? styles.activeTab : styles.pointer} onClick={()=>setSerfType(1)}>
+                    <div class="form-check form-switch form-switch-success">
+                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                    </div>
+                    <div className='px-2' onClick={()=>setSerfType(1)}>
                         <span>Dine Out</span>
                     </div>
                 </div>
@@ -41,6 +59,17 @@ export default function Home(){
                     </div>
                 </div>
             </div>
+            <Slider {...settings} className='container rounded mt-3'>
+                <div>
+                    <Image src={banner1} height={250} className='w-100 rounded bg-warning'/>
+                </div>
+                {/* <div>
+                    <Image src={banner2} height={250} className='w-100 rounded bg-warning'/>
+                </div> */}
+                <div>
+                    <Image src={banner3} height={250} className='w-100 rounded bg-warning'/>
+                </div>
+            </Slider>
         </>
     )
 }

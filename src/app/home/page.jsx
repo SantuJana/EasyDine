b308fb3a5bef0514107a8e4f4525a390efc73955
santu jana <santujana.1998@gmail.com>
2024-04-04 22:45:12 +0530

    category

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -12,6 +12,7 @@ import "slick-carousel/slick/slick-theme.css";
 import banner1 from '../../../public/images/banner/1.jpg'
 import banner2 from '../../../public/images/banner/2.svg'
 import banner3 from '../../../public/images/banner/3.jpg'
+import category from '@/config/category';
 
 export default function Home(){
     const [serfType, setSerfType] = useState(0);
@@ -36,8 +37,8 @@ export default function Home(){
                     <div className='px-2' onClick={()=>setSerfType(0)}>
                         <span>Order Online</span>
                     </div>
-                    <div class="form-check form-switch form-switch-success">
-                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                    <div className="form-check form-switch form-switch-success">
+                        <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
                     </div>
                     <div className='px-2' onClick={()=>setSerfType(1)}>
                         <span>Dine Out</span>
@@ -70,6 +71,21 @@ export default function Home(){
                     <Image src={banner3} height={250} className='w-100 rounded bg-warning'/>
                 </div>
             </Slider>
+
+            <div className="container mt-5">
+                <h4 style={{color: '#555555'}}>Shop By Category</h4>
+                <div className='d-flex gap-5 overflow-auto'>
+                    {
+                        category.map( (element)=>
+                        <div className='d-flex flex-column align-items-center'>
+                            <div className='bg-warning d-flex justify-content-center align-items-center rounded rounded-circle' style={{height: 100, width: 100}}>
+                                <Image src={require(`../../../public/images/category/${element.image}`)} width={80} height={80} />
+                            </div>
+                            <span className='text-center'>{element.title}</span>
+                        </div>)
+                    }
+                </div>
+            </div>
         </>
     )
 }
---
+++ src/config/category.js
@@ -0,0 +1,40 @@
+const category = [
+    {
+        title: "Burger",
+        image: "burger.png"
+    },
+    {
+        title: "Platter",
+        image: "platter.png"
+    },
+    {
+        title: "Noddles",
+        image: "noddles.png"
+    },
+    {
+        title: "Pizza",
+        image: "pizza.png"
+    },
+    {
+        title: "Sea Foods",
+        image: "sea food.png"
+    },
+    {
+        title: "Juice",
+        image: "juice.png"
+    },
+    {
+        title: "Cake",
+        image: "cakes.png"
+    },
+    {
+        title: "Coffee & Soft Drinks",
+        image: "coffee.png"
+    },
+    {
+        title: "Ice Cream",
+        image: "ice cream.png"
+    },
+]
+
+export default category;
